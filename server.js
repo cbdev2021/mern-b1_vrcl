1c1cd924c0e57e071049a4f8401582e7d3e1a86f
carlos <cbdev2021@gmail.com>
2023-08-30 04:07:03 -0400

    se agrega https localhost 3000

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ app.use((req, res, next) => {
 app.use(cors({
   origin: [
     '*',
+    'http://localhost:3000',
     'https://localhost:3000',
     'https://react-simple.onrender.com',
     'https://mern-f1.onrender.com',
